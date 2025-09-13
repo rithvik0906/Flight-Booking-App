d994f5906c4f96e10c0d8d246c46daa2375b0680
rithvik0906 <rithvik0906@gmail.com>
2025-09-13 15:33:30 +0530

    Fixed background image path for GitHub Pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
       style={{
         minHeight: "100vh",
         width: "100vw",
-        backgroundImage: "url('/plane.jpg')",
+        backgroundImage: `url(${process.env.PUBLIC_URL}/plane.jpg)`,
         backgroundPosition: "center center",
         backgroundRepeat: "no-repeat",
         backgroundAttachment: "fixed",
